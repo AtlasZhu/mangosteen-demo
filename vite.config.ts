d0adbc8bfdfcc7e666cf51f15e30cb2bd86b30c8
AtlasZhu <atlaszhu@foxmail.com>
2023-07-31 17:28:39 +0800

    修改viteconfig，固定端口为3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/mangosteen-demo/",
+  base: "/mangosteen-demo/",
   plugins: [
     vue(),
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
-      transformOn:true,
-      mergeProps:true
+      transformOn: true,
+      mergeProps: true,
     }),
-  ]
-})
+  ],
+  server: {
+    host: "localhost", //ip地址
+    port: 3000, // 设置服务启动端口号
+    open: false, // 设置服务启动时是否自动打开浏览器
+  },
+});
