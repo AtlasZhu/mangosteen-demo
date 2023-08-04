436132ac06c09bd4058bcb398cdef883b1ebc77b
AtlasZhu <atlaszhu@foxmail.com>
2023-08-04 14:51:52 +0800

    在welcome页使用节流函数

---
+++ src/hooks/throttle.tsx
@@ -0,0 +1,13 @@
+export const throttle = (fn: Function, time: number) => {
+  let timer: number | null;
+  return (...args: any) => {
+    if (timer) {
+      return;
+    } else {
+      timer = setTimeout(() => {
+        timer = null;
+      }, time);
+      return fn(...args);
+    }
+  };
+};
--- src/views/Start.tsx
+++ src/views/Start.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, ref } from "vue";
+import { throttle } from "../hooks/throttle";
 import { useSwipe } from "../hooks/useSwipe";
 export const Start = defineComponent({
   setup() {
@@ -6,21 +7,28 @@ export const Start = defineComponent({
     const { swiping, distance, direction } = useSwipe(ele, {
       beforeStart: (e) => e.preventDefault(),
     });
+    const click = throttle(() => {
+      console.log("hihi");
+    }, 3000);
     // watchEffect(() => {
     //   console.log(swiping.value, distance.value, direction.value);
     // });
     return () => (
-      <div ref={ele}>
-        你好
-        <hr />
-        {JSON.stringify(swiping.value)}
-        <hr />
-        {JSON.stringify(distance.value)}
-        <hr />
-        {direction.value}
-        <hr />
-        这是开始页面
-      </div>
+      <>
+        <div ref={ele}>
+          你好
+          <hr />
+          {JSON.stringify(swiping.value)}
+          <hr />
+          {JSON.stringify(distance.value)}
+          <hr />
+          {direction.value}
+          <hr />
+          这是开始页面
+          <hr />
+        </div>
+        <div onClick={click}>loghi</div>
+      </>
     );
   },
 });
