3a30f4d9d274b2297d81756afe8a22a96b17bbb4
AtlasZhu <atlaszhu@foxmail.com>
2023-10-03 00:15:03 +0800

    制作ItemList自定义时间Overlay页面的样式

--- src/components/itemPage/ItemList.tsx
+++ src/components/itemPage/ItemList.tsx
@@ -1,52 +1,73 @@
-import { defineComponent, ref } from "vue";
+import { Overlay } from "vant";
+import { defineComponent, ref, watchEffect } from "vue";
 import svgMenu from "../../assets/icons/menu.svg";
 import { MainLayout } from "../../layouts/MainLayout";
+import { Button } from "../../shared/Button";
 import { Icon } from "../../shared/Icon";
 import { Tab, Tabs } from "../../shared/Tabs";
 import { Time } from "../../shared/time";
+import s from "./ItemList.module.scss";
 import { ItemSummary } from "./ItemSummary";
 export const ItemList = defineComponent({
   setup() {
     const refSelected = ref("本月");
     const time = new Time();
     const timeList = [
-      { start: time.firstDayOfMonth(), end: time.lastDayOfMonth() },
-      { start: time.add(-1, "month").firstDayOfMonth(), end: time.add(-1, "month").lastDayOfMonth() },
-      { start: time.firstDayOfYear(), end: time.lastDayOfYear() },
+      { start: time.firstDayOfMonth().formatAsString(), end: time.lastDayOfMonth().formatAsString() },
+      {
+        start: time.add(-1, "month").firstDayOfMonth().formatAsString(),
+        end: time.add(-1, "month").lastDayOfMonth().formatAsString(),
+      },
+      { start: time.firstDayOfYear().formatAsString(), end: time.lastDayOfYear().formatAsString() },
     ];
-    const customTime = ref({ start: new Time(), end: new Time() });
+    const customTime = ref({ start: new Time().formatAsString(), end: new Time().formatAsString() });
+    const refOverlayVisible = ref(false);
+    watchEffect(() => {
+      if (refSelected.value === "自定义时间") refOverlayVisible.value = true;
+    });
     return () => (
       <MainLayout>
         {{
           icon: () => <Icon iconName={svgMenu} />,
           title: () => "菠萝账本",
           content: () => (
-            <Tabs v-model:selected={refSelected.value}>
-              <Tab name="本月">
-                <ItemSummary
-                  startTime={timeList[0].start.formatAsString()}
-                  endTime={timeList[0].end.formatAsString()}
-                />
-              </Tab>
-              <Tab name="上月">
-                <ItemSummary
-                  startTime={timeList[1].start.formatAsString()}
-                  endTime={timeList[1].end.formatAsString()}
-                />
-              </Tab>
-              <Tab name="今年">
-                <ItemSummary
-                  startTime={timeList[2].start.formatAsString()}
-                  endTime={timeList[2].end.formatAsString()}
-                />
-              </Tab>
-              <Tab name="自定义时间">
-                <ItemSummary
-                  startTime={customTime.value.start.formatAsString()}
-                  endTime={customTime.value.end.formatAsString()}
-                />
-              </Tab>
-            </Tabs>
+            <>
+              <Tabs v-model:selected={refSelected.value}>
+                <Tab name="本月">
+                  <ItemSummary startTime={timeList[0].start} endTime={timeList[0].end} />
+                </Tab>
+                <Tab name="上月">
+                  <ItemSummary startTime={timeList[1].start} endTime={timeList[1].end} />
+                </Tab>
+                <Tab name="今年">
+                  <ItemSummary startTime={timeList[2].start} endTime={timeList[2].end} />
+                </Tab>
+                <Tab name="自定义时间">
+                  <ItemSummary startTime={customTime.value.start} endTime={customTime.value.end} />
+                </Tab>
+              </Tabs>
+              <Overlay show={refOverlayVisible.value} class={s.overlayWrapper}>
+                <div class={s.overlayContent}>
+                  <header>请选择时间</header>
+                  <main>
+                    <label>
+                      开始时间: <input v-model={customTime.value.start}></input>
+                    </label>
+                    <label>
+                      结束时间: <input v-model={customTime.value.end}></input>
+                    </label>
+                  </main>
+                  <footer>
+                    <Button onClick={() => (refOverlayVisible.value = false)} class={s.cancel}>
+                      取消
+                    </Button>
+                    <Button onClick={() => (refOverlayVisible.value = false)} class={s.confirm}>
+                      确定
+                    </Button>
+                  </footer>
+                </div>
+              </Overlay>
+            </>
           ),
         }}
       </MainLayout>
