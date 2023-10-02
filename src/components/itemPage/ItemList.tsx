b23212db25d85a4cec014fdf6b2e9d408e710654
AtlasZhu <atlaszhu@foxmail.com>
2023-10-02 15:18:18 +0800

    wip: 制作ItemList页面

--- src/components/itemPage/ItemList.tsx
+++ src/components/itemPage/ItemList.tsx
@@ -1,7 +1,26 @@
-import { defineComponent } from "vue";
-
+import { defineComponent, ref } from "vue";
+import svgMenu from "../../assets/icons/menu.svg";
+import { MainLayout } from "../../layouts/MainLayout";
+import { Icon } from "../../shared/Icon";
+import { Tab, Tabs } from "../../shared/Tabs";
 export const ItemList = defineComponent({
   setup() {
-    return () => <>这是item list页面</>;
+    const refSelected = ref("本月");
+    return () => (
+      <MainLayout>
+        {{
+          icon: () => <Icon iconName={svgMenu} />,
+          title: () => "菠萝账本",
+          content: () => (
+            <Tabs v-model:selected={refSelected.value}>
+              <Tab name="本月"></Tab>
+              <Tab name="上月"></Tab>
+              <Tab name="今年"></Tab>
+              <Tab name="自定义时间"></Tab>
+            </Tabs>
+          ),
+        }}
+      </MainLayout>
+    );
   },
 });
