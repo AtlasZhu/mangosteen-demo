aee63a1caa875aa4eaf4136fbec22517dafdae85
AtlasZhu <atlaszhu@foxmail.com>
2023-10-04 14:06:39 +0800

    wip: 制作图表页面

--- src/views/StatisiticsPage.tsx
+++ src/views/StatisiticsPage.tsx
@@ -1,6 +1,8 @@
 import { defineComponent } from "vue";
+import { Charts } from "../components/statistics/Charts";
+import { TimeTabsLayout } from "../layouts/TimeTabsLayout";
 export const StatisticsPage = defineComponent({
   setup() {
-    return () => <></>;
+    return () => <TimeTabsLayout component={Charts}></TimeTabsLayout>;
   },
 });
