690b91a1a107917d28122e7216aa2f499534b605
AtlasZhu <atlaszhu@foxmail.com>
2023-10-04 16:37:11 +0800

    创建Charts组件

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,10 +1,22 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import s from "./Charts.module.scss";
 export const Charts = defineComponent({
   props: {
     startTime: { type: String, required: true },
     endTime: { type: String, required: true },
   },
   setup() {
-    return () => <>这里呈现各种图标</>;
+    const kind = ref("expenses");
+    return () => (
+      <>
+        <div class={s.selectWrapper}>
+          类型:
+          <select v-model={kind.value}>
+            <option value="expenses">支出</option>
+            <option value="income">收入</option>
+          </select>
+        </div>
+      </>
+    );
   },
 });
