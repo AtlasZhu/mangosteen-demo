6af299ee86be97716095d836db27cc841506ef6c
AtlasZhu <atlaszhu@foxmail.com>
2023-10-09 20:42:04 +0800

    可以发送删除标签的请求了

--- src/components/tags/TagsEdit.tsx
+++ src/components/tags/TagsEdit.tsx
@@ -1,11 +1,36 @@
+import { showConfirmDialog, showDialog } from "vant";
 import { defineComponent } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import { MainLayout } from "../../layouts/MainLayout";
 import { BackIcon } from "../../shared/BackIcon";
 import { Button } from "../../shared/Button";
+import { http } from "../../shared/Http";
 import { TagForm } from "./TagForm";
 import s from "./TagForm.module.scss";
 export const TagsEdit = defineComponent({
   setup() {
+    const route = useRoute();
+    const router = useRouter();
+    const id = route.params.id;
+    if (!id) return <div>此Tag的ID不存在</div>;
+    const onDelete = (options?: { withItems?: boolean }) => {
+      showConfirmDialog({
+        title: "删除标签",
+        message: "你确定要删除吗？",
+      })
+        .then(() => {
+          const id = route.params.id;
+          http
+            .delete(`/tags/${id.toString()}`, { withItems: options?.withItems ?? false })
+            .then(() => {
+              router.back();
+            })
+            .catch(() => {
+              showDialog({ title: "错误", message: "删除失败" });
+            });
+        })
+        .catch();
+    };
     return () => (
       <MainLayout>
         {{
@@ -15,10 +40,10 @@ export const TagsEdit = defineComponent({
             <>
               <TagForm />
               <div class={s.actions}>
-                <Button class={s.deleteTag} level="danger">
+                <Button class={s.deleteTag} level="danger" onClick={() => onDelete()}>
                   删除标签
                 </Button>
-                <Button class={s.deleteTagAndRecord} level="danger">
+                <Button class={s.deleteTagAndRecord} level="danger" onClick={() => onDelete({ withItems: true })}>
                   删除标签和记账
                 </Button>
               </div>
