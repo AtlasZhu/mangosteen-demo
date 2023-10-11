9ac1299b4857e313b9e5439cb9f42699d7a10445
AtlasZhu <atlaszhu@foxmail.com>
2023-10-11 13:02:57 +0800

    封装lineChart

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,16 +1,36 @@
-import { defineComponent, ref } from "vue";
+import { computed, defineComponent, onMounted, ref } from "vue";
+import { http } from "../../shared/Http";
 import { BarChart } from "./BarChart";
 import s from "./Charts.module.scss";
 import { LineChart } from "./LineChart";
 import { PieChart } from "./PieChart";
 
+type Data1Item = { happen_at: string; amount: number };
+type Data1 = Data1Item[];
 export const Charts = defineComponent({
   props: {
     startTime: { type: String },
     endTime: { type: String },
   },
-  setup() {
+  setup(props) {
     const kind = ref("expenses");
+    const data1 = ref<Data1>([]);
+    const betterData1 = computed<[string, number][]>(() => data1.value.map(item => [item.happen_at, item.amount]));
+    console.log(props.startTime, props.endTime);
+    onMounted(() => {
+      http
+        .get<{ groups: Data1; summary: number }>("/items/summary", {
+          happen_after: props.startTime,
+          happen_before: props.endTime,
+          kind: kind.value,
+        })
+        .then(response => {
+          console.log("response.data:", response.data);
+          data1.value = response.data.groups;
+        })
+        .catch();
+    });
+
     return () => (
       <div class={s.wrapper}>
         <div class={s.selectWrapper}>
@@ -21,7 +41,7 @@ export const Charts = defineComponent({
           </select>
         </div>
         <div class={s.chartsWrapper}>
-          <LineChart class={s.lineChart}></LineChart>
+          <LineChart class={s.lineChart} data={betterData1.value}></LineChart>
           <PieChart class={s.pieChart}></PieChart>
           <BarChart></BarChart>
         </div>
--- src/components/statistics/LineChart.tsx
+++ src/components/statistics/LineChart.tsx
@@ -1,92 +1,68 @@
 import * as echarts from "echarts";
-import { defineComponent, onMounted, ref } from "vue";
+import { PropType, defineComponent, onMounted, ref, watch } from "vue";
 import { Time } from "../../shared/time";
 import { getMoney } from "../../shared/utils";
 
+const echartsOptions = {
+  tooltip: {
+    show: true,
+    trigger: "axis",
+    formatter: ([item]: any) => {
+      const [x, y] = item.data;
+      return `${new Time(new Date(x)).formatAsString("YYYY年MM月DD日")} ￥${getMoney(y)}`;
+    },
+  },
+  grid: [{ left: 16, top: 20, right: 16, bottom: 20 }],
+  xAxis: {
+    type: "time",
+    boundaryGap: ["3%", "0%"],
+    axisLabel: {
+      formatter: (value: string) => new Time(new Date(value)).formatAsString("MM-DD"),
+    },
+    axisTick: {
+      alignWithLabel: true,
+    },
+  },
+  yAxis: {
+    show: true,
+    type: "value",
+    splitLine: {
+      show: true,
+      lineStyle: {
+        type: "dashed",
+      },
+    },
+    axisLabel: {
+      show: false,
+    },
+  },
+};
 export const LineChart = defineComponent({
-  setup() {
-    const refChart = ref<HTMLDivElement>();
-    const echartsOptions = {
-      tooltip: {
-        show: true,
-        trigger: "axis",
-        formatter: ([item]: any) => {
-          const [x, y] = item.data;
-          return `${new Time(new Date(x)).formatAsString("YYYY年MM月DD日")} ￥${getMoney(y)}`;
-        },
-      },
-      grid: [{ left: 16, top: 20, right: 16, bottom: 20 }],
-      xAxis: {
-        type: "time",
-        boundaryGap: ["3%", "0%"],
-        axisLabel: {
-          formatter: (value: string) => new Time(new Date(value)).formatAsString("MM-DD"),
-        },
-        axisTick: {
-          alignWithLabel: true,
-        },
-      },
-      yAxis: {
-        show: true,
-        type: "value",
-        splitLine: {
-          show: true,
-          lineStyle: {
-            type: "dashed",
-          },
-        },
-        axisLabel: {
-          show: false,
-        },
-      },
-    };
-    const data = [
-      ["2018-01-01T00:00:00.000+0800", 150],
-      ["2018-01-02T00:00:00.000+0800", 230],
-      ["2018-01-03T00:00:00.000+0800", 224],
-      ["2018-01-04T00:00:00.000+0800", 218],
-      ["2018-01-05T00:00:00.000+0800", 135],
-      ["2018-01-06T00:00:00.000+0800", 147],
-      ["2018-01-07T00:00:00.000+0800", 260],
-      ["2018-01-08T00:00:00.000+0800", 300],
-      ["2018-01-09T00:00:00.000+0800", 200],
-      ["2018-01-10T00:00:00.000+0800", 300],
-      ["2018-01-11T00:00:00.000+0800", 400],
-      ["2018-01-12T00:00:00.000+0800", 500],
-      ["2018-01-13T00:00:00.000+0800", 400],
-      ["2018-01-14T00:00:00.000+0800", 300],
-      ["2018-01-15T00:00:00.000+0800", 200],
-      ["2018-01-16T00:00:00.000+0800", 100],
-      ["2018-01-17T00:00:00.000+0800", 200],
-      ["2018-01-18T00:00:00.000+0800", 300],
-      ["2018-01-19T00:00:00.000+0800", 400],
-      ["2018-01-20T00:00:00.000+0800", 500],
-      ["2018-01-21T00:00:00.000+0800", 600],
-      ["2018-01-22T00:00:00.000+0800", 700],
-      ["2018-01-23T00:00:00.000+0800", 800],
-      ["2018-01-24T00:00:00.000+0800", 900],
-      ["2018-01-25T00:00:00.000+0800", 1000],
-      ["2018-01-26T00:00:00.000+0800", 1100],
-      ["2018-01-27T00:00:00.000+0800", 1200],
-      ["2018-01-28T00:00:00.000+0800", 1300],
-      ["2018-01-29T00:00:00.000+0800", 1400],
-      ["2018-01-30T00:00:00.000+0800", 1500],
-      ["2018-01-31T00:00:00.000+0800", 1600],
-    ];
+  props: { data: { type: Array as PropType<[string, number][]>, required: true } },
+  setup(props) {
+    const refDiv = ref<HTMLDivElement>();
+    let refChart: echarts.ECharts;
 
     onMounted(() => {
-      if (!refChart.value) return console.log("ref绑定失败");
-      echarts.init(refChart.value).setOption({
+      if (!refDiv.value) return console.log("ref绑定失败");
+      refChart = echarts.init(refDiv.value);
+      refChart.setOption({
         ...echartsOptions,
         series: [
           {
-            data: data,
+            data: props.data,
             type: "line",
           },
         ],
       });
       console.log("init");
     });
-    return () => <div ref={refChart}></div>;
+    watch(
+      () => props.data,
+      () => {
+        refChart.setOption({ series: [{ data: props.data }] });
+      },
+    );
+    return () => <div ref={refDiv}></div>;
   },
 });
